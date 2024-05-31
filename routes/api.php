1b0a4c4a71332471c92df5810623314767ca13a5
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-05-31 11:18:32 +0300

    Commit Changes Made on the routes

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,8 @@
 
 use App\Http\Controllers\BranchController;
 use App\Http\Controllers\BranchUserController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\CodeController;
-
 use App\Http\Controllers\PurchaseController;
-
-use App\Http\Controllers\PurchaseController;
-use App\Http\Controllers\CodeController;
 use App\Http\Controllers\InsuranceController;
 use App\Http\Controllers\GetPurchaseListController;
 use App\Http\Controllers\MapImportedItemController;
@@ -22,11 +15,12 @@ use App\Http\Controllers\GetImportedItemInformationController;
 use App\Http\Controllers\CompositionItemController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\InitializationController;
-use App\Http\Controllers\InsuranceController;
 use App\Http\Controllers\ItemClassificationController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ItemOpeningStockController;
 use App\Http\Controllers\NoticeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -38,41 +32,31 @@ use App\Http\Controllers\NoticeController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('GetCodeList', CodeController::class);
 
-Route::post('/AddInsurance', [InsuranceController::class, 'addInsurance']);
-// Route::post('/AddInsurance', [InsuranceController::class, 'addInsurance'])->middleware('apikey');
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-
- Route::middleware('auth:api')->get('/user', function (Request $request) {
-
-    return $request->user();
-});
-
-Route::apiResource('insurance', InsuranceController::class);
-Route::apiResource('purchase', PurchaseController::class);
-Route::apiResource('GetImportedItemInformation', GetImportedItemInformationController::class);
-Route::apiResource('GetPurchaseList', GetPurchaseListController::class);
-Route::apiResource('MapImportedItem', MapImportedItemController::class);
-
-Route::apiResource('MapPurchase', MappingPurchaseController::class);
-Route::apiResource('UpdateMapPurchaseStatus', UpdateMapPurchaseStatusController::class);
-Route::apiResource('SearchByDate', MapPurchaseSearchByDateController::class);
-Route::apiResource('AddDirectCreditNote', AddDirectCreditNoteController::class);
-
-Route::apiResource('GetCodeList', CodeController::class);
-Route::post('/AddInsurance', [InsuranceController::class, 'addInsurance']);
-// Route::post('/AddInsurance', [InsuranceController::class, 'addInsurance'])->middleware('apikey');
 Route::apiResource('GetCodeList', CodeController::class)->only([
     'index',
     'store',
     'update',
     'destroy'
 ]);
+
+Route::post('/AddInsurance', [InsuranceController::class, 'addInsurance']);
+// Route::post('/AddInsurance', [InsuranceController::class, 'addInsurance'])->middleware('apikey');
+
+Route::apiResource('insurance', InsuranceController::class);
+Route::apiResource('purchase', PurchaseController::class);
+Route::apiResource('GetImportedItemInformation', GetImportedItemInformationController::class);
+Route::apiResource('GetPurchaseList', GetPurchaseListController::class);
+Route::apiResource('MapImportedItem', MapImportedItemController::class);
+Route::apiResource('MapPurchase', MappingPurchaseController::class);
+Route::apiResource('UpdateMapPurchaseStatus', UpdateMapPurchaseStatusController::class);
+Route::apiResource('SearchByDate', MapPurchaseSearchByDateController::class);
+Route::apiResource('AddDirectCreditNote', AddDirectCreditNoteController::class);
+
 Route::apiResource('GetNoticeList', NoticeController::class)->only([
     'index',
     'store',
@@ -127,4 +111,3 @@ Route::apiResource('ItemOpeningStock', ItemOpeningStockController::class)->only(
     'update',
     'destroy'
 ]);
-Route::post('/AddInsurance', [InsuranceController::class, 'addInsurance']);
